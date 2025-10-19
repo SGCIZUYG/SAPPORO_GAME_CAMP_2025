79f0d91d53b2816d5e2a6e6611130424513ed0a0
MattsGrate <songcunshuu23@gmail.com>
2025-10-19 09:41:55 +0900

    背景とHPのUIを追加しました

--- Assets/Scripts/HPLost.cs
+++ Assets/Scripts/HPLost.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class HPLost : MonoBehaviour
 {
     [SerializeField] private int _hpNum = 0;
-    [SerializeField] private PlayerDamage _pDmagage = default;
+    [SerializeField] private PlayerDamage _pDmageCount = default;
     private SpriteRenderer _sp = default;
 
     private void Start()
@@ -12,7 +12,7 @@ public class HPLost : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if(_pDmagage._damageCount == _hpNum)
+        if(_pDmageCount._damageCount == _hpNum)
         {
             _sp.color = Color.black;
         }
