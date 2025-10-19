1983b06bd00666b8e5e1a72282e0cc466030931b
thikarih-ctrl <t.hikari.h@gmail.com>
2025-10-19 14:30:17 +0900

    コメ入れ

--- Assets/Scripts/FukidasiSelect.cs
+++ Assets/Scripts/FukidasiSelect.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,7 +11,15 @@ public class FukidasiSelect : MonoBehaviour
     [SerializeField] private GameObject _fukidasi4 = default;
     [SerializeField] private GameObject _fukidasi5 = default;
     [SerializeField] private EnemyDamage _eDamage = default;
+    [SerializeField] TextMeshProUGUI _mainTextObject;
     private UserScriptManager _usManager = default;
+
+    List<string> word_1 = new List<string> { "君は本当に使えないな。", "", "" };
+    List<string> word_2 = new List<string> { "休日出勤も前提で、他のメンバーは手を貸さない。君ならできるだろ？", "来月のプロジェクトの資料を全部作り直してもらう。休日出勤も前提で、他のメンバーは手を貸さない。", "" };
+    List<string> word_3 = new List<string> { "じゃあ、やれないなら代わり探すけど。どうする？", "", "" };
+    List<string> word_4 = new List<string> { "君のそれじゃ、将来どうなるか分からないからな。", "", "" };
+    List<string> word_5 = new List<string> { "逃げるな、ちゃんとついてこい。分かったな？", "", "昨日の飲み会で君、あまり発言なかったけど何を考えてたの？…将来どうなるか分からないからな。" };
+    int count;
 
     private void Start()
     {
@@ -19,6 +29,7 @@ public class FukidasiSelect : MonoBehaviour
         _fukidasi4.SetActive(false);
         _fukidasi5.SetActive(false);
         _usManager = GetComponent<UserScriptManager>();
+        count = 0;
     }
 
     private void Update()
@@ -39,18 +50,97 @@ public class FukidasiSelect : MonoBehaviour
             switch (fukidasiNum)
             {
                 case 0:
-                    _eDamage._damageCount++;
+                    if(count ==1)
+                    {
+                        _mainTextObject.text = word_1[0];
+                        count++;
+                    }
+                    else if(count == 2)
+                        {
+                        _mainTextObject.text = word_1[1];
+                        count++;
+                    }
+                    else if (count == 3)
+                    {
+                        _mainTextObject.text = word_1[2];
+                        count++;
+                    }
+                        _eDamage._damageCount++;
                     break;
+
                 case 1:
+                    if (count == 1)
+                    {
+                        _mainTextObject.text = word_2[0];
+                        count++;
+                    }
+                    else if (count == 2)
+                    {
+                        _mainTextObject.text = word_2[1];
+                        count++;
+                    }
+                    else if (count == 3)
+                    {
+                        _mainTextObject.text = word_2[2];
+                        count++;
+                    }
                     _eDamage._damageCount++;
                     break;
+
                 case 2:
+                    if (count == 1)
+                    {
+                        _mainTextObject.text = word_3[0];
+                        count++;
+                    }
+                    else if (count == 2)
+                    {
+                        _mainTextObject.text = word_3[1];
+                        count++;
+                    }
+                    else if (count == 3)
+                    {
+                        _mainTextObject.text = word_3[2];
+                        count++;
+                    }
                     _eDamage._damageCount++;
                     break;
+
                 case 3:
+                    if (count == 1)
+                    {
+                        _mainTextObject.text = word_4[0];
+                        count++;
+                    }
+                    else if (count == 2)
+                    {
+                        _mainTextObject.text = word_4[1];
+                        count++;
+                    }
+                    else if (count == 3)
+                    {
+                        _mainTextObject.text = word_4[2];
+                        count++;
+                    }
                     _eDamage._damageCount++;
                     break;
+
                 case 4:
+                    if (count == 1)
+                    {
+                        _mainTextObject.text = word_5[0];
+                        count++;
+                    }
+                    else if (count == 2)
+                    {
+                        _mainTextObject.text = word_5[1];
+                        count++;
+                    }
+                    else if (count == 3)
+                    {
+                        _mainTextObject.text = word_5[2];
+                        count++;
+                    }
                     _eDamage._damageCount++;
                     break;
             }
